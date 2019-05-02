c1def7fcdeaeb0a00c8e427a46c99e2c80029dab
Busher <bridibusher@gmail.com>
2019-05-01 21:31:26 -0700

    fixed some code with subprograms

--- program/pizza3.py
+++ program/pizza3.py
@@ -5,17 +5,16 @@ email = "bridibusher@gmail.com"
 phone = "5598273708"
 customer = Customer(firstName, lastName, email, phone)
 street = "5842 W buena vista ave"
-city = "visalia")
+city = "visalia"
 state = "CA"
 zip = "93919"
 address = Address(street, city, state, zip)
-store = adress.closest_store()
-order = order(store, customer, adress)
+store = address.closest_store()
+order = Order(store, customer, address)
 cardNumber = "123212212"
 cardExpDate = "4654"
 cardSecNum = "551"
 cardZip = "93291"
 card = PaymentObject(cardNumber, cardExpDate, cardSecNum, cardZip)
-order.add_item("DDBDS")
+order.add_item("W08PMANW")
 order.place(card)
-
---
+++ program/pizzaorder.py
@@ -0,0 +1,37 @@
+from pizzapi import *
+
+print("Pizza Time!")
+firstName = raw_input("Enter first name: ")
+lastName = raw_input("Enter last name: ")
+email = raw_input("Enter email: ")
+phone = raw_input("Enter phone number: ")
+print("Creating customer with " + firstName +
+      ", " + lastName + ", " + email + ", " + phone)
+customer = Customer(firstName, lastName, email, phone)
+street = raw_input("Enter street: ")
+city = raw_input("Enter city: ")
+state = raw_input("Enter state (ABV): ")
+zip = raw_input("Enter zip code: ")
+print("using adress: " + street + ", " + city + ", " + state + ", " + zip)
+address = Address(street, city, state, zip)
+store = address.closest_store()
+menu = store.get_menu()
+order = Order(store, customer, address)
+loopChoice = 'y'
+while loopChoice == 'y':
+    search = raw_input("Enter query to search menu: ")
+    print(menu.search(Name=search))
+    print("-----------------")
+    orderchoice = raw_input("would you like to add an item to order? (y/n) ")
+    while orderchoice == 'y':
+        orderItem = raw_input("Input item to place on order: ")
+        order = Order(store, customer, address)
+        order.add_item(orderItem)
+        orderchoice = raw_input("Add another item? (y/n) ")
+    loopChoice = raw_input("search again? (y/n) ")
+cardNumber = raw_input("Enter credit card number: ")
+cardExpDate = raw_input("Enter credit card exp date (ex: 0115): ")
+cardSecNum =  raw_input("Enter credit card security number: ")
+cardZip = raw_input("Enter credit card billing zip code: ")
+card = PaymentObject(cardNumber, cardExpDate, cardSecNum, cardZip)
+order.place(card)
